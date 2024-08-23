fc494ee83488e7e6559e938e4fac96264f843dda
jonathan <jonathan.ruiz.1793@gmail.com>
2024-08-22 19:18:46 -0500

    Made required changes to components

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ import { CharacterCardComponent } from "./components/CharacterCardsComponent";
 import { data } from "../characterData";
 
 function App() {
-  let firstFiveByVotes = data;
+  let firstFiveByVotes = [...data];
   firstFiveByVotes = firstFiveByVotes
-    .slice(0, 5)
-    .sort((a, b) => b.votes - a.votes);
+    .sort((a, b) => b.votes - a.votes)
+    .slice(0, 5);
+
+  /**About the Character Card Component. I don't actually know what you wanted me to do with that one.
+   *
+   * you said in the video that the characters in the card component shouldn't be in perfect alphabetical order. but they are displayed in
+   * the order that the array is originally set up in CharacterData.ts file without any alteration. they are also in the same order as the "Original Example showed"
+   *
+   * also! in the ReadMe File of the original project it states in the final step of the check list that the character cards should not be sorted. Am I missing something?
+   * this got me confused.
+   *
+   * I fixed the other things but please let me know what else need to be changed. Thanks for grading my project!
+   */
+
   return (
     <>
       <NavBarComponent />
--- src/components/CharacterRatingComponent.tsx
+++ src/components/CharacterRatingComponent.tsx
@@ -9,18 +9,16 @@ type CharacterType = {
 
 export class CharacterRatingComponent extends Component<CharacterType> {
   render() {
-    const { name, skillset, votes } = this.props.character;
+    const { name, skillset, votes, nickName } = this.props.character;
     const { id } = this.props;
 
     return (
       <>
         <tr className={id % 2 === 0 ? "dark" : "light"}>
-          <td className="table-cell-1">{name}</td>
-          <td className="table-cell-2">
-            {skillset.map((item, index) =>
-              index === skillset.length - 1 ? `${item}.` : `${item}, `
-            )}
+          <td className="table-cell-1">
+            {name} {nickName !== null ? `"${nickName}"` : ""}
           </td>
+          <td className="table-cell-2">{skillset.join(", ")}</td>
           <td className="table-cell-3">{votes}</td>
         </tr>
       </>
